cd29daf85990b52d3ffae5ded2cdbf93e8f9716f
Krishnan Singaram <ankrishnadev@gmail.com>
2025-02-01 12:59:43 +0530

    Added PokemonViewModel.swift

---
+++ Dex3/ViewModel/PokemonViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  PokemonViewModel.swift
+//  Dex3
+//
+//  Created by Krishnan Singaram on 01/02/25.
+//
+
+import Foundation
+
+@MainActor
+class PokemonViewModel: ObservableObject {
+    enum State {
+        case notStarted
+        case fetching
+        case success
+        case failed(error: Error)
+    }
+
+    @Published var state: State = .notStarted
+
+    private let controller: FetchController
+
+    init(controller: FetchController) {
+        self.controller = controller
+        
+        Task {
+            await getPokemon()
+        }
+    }
+
+    private func getPokemon() async {
+        state = .fetching
+
+        do {
+            var pokedex = try await controller.fetchAllPokemons()
+            pokedex.sort { $0.id < $1.id }
+
+            for pokemon in pokedex {
+                let newPokemon = Pokemon(
+                    context: PersistenceController.shared.container
+                        .viewContext)
+                
+                newPokemon.id = Int16(pokemon.id)
+                newPokemon.name = pokemon.name
+                newPokemon.types = pokemon.types as NSObject
+                newPokemon.hp = Int16(pokemon.hp)
+                newPokemon.attack = Int16(pokemon.attack)
+                newPokemon.defence = Int16(pokemon.defence)
+                newPokemon.specialAttack = Int16(pokemon.specialAttack)
+                newPokemon.specialDefence = Int16(pokemon.specialDefence)
+                newPokemon.speed = Int16(pokemon.speed)
+                newPokemon.sprite = pokemon.sprite
+                newPokemon.shiny = pokemon.shiny
+                newPokemon.favorite = false
+                
+                try PersistenceController.shared.container.viewContext.save()
+                
+            }
+            
+            state = .success
+        } catch {
+            state = .failed(error: error)
+        }
+    }
+}
