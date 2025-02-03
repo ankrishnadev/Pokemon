efb971f8eb8b7fde6709f570cbcd63f4c3990845
Krishnan Singaram <ankrishnadev@gmail.com>
2025-02-03 09:37:59 +0530

    Added SamplePokemon

---
+++ Dex3/PokemonDetailView.swift
@@ -0,0 +1,68 @@
+//
+//  PokemonDetailView.swift
+//  Dex3
+//
+//  Created by Krishnan Singaram on 02/02/25.
+//
+
+import CoreData
+import SwiftUI
+
+struct PokemonDetailView: View {
+
+    @EnvironmentObject var pokemon: Pokemon
+    @State var showShinny = false
+
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Image("normalgrasselectricpoisonfairy")
+                    .resizable()
+                    .scaledToFit()
+
+                AsyncImage(url: pokemon.sprite) { image in
+                    image.resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 10)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .padding([.leading, .trailing], 30)
+                        .padding([.top, .bottom], 10)
+                        .background(Color(type.capitalized))
+                        .clipShape(Capsule())
+                }
+
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    showShinny.toggle()
+                } label: {
+                    if showShinny {
+                        Image(systemName: "wand.and.stars")
+                    } else {
+                        Image(systemName: "wand.and.sparkles.inverse")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView().environmentObject(SamplePokemon.samplePokemon)
+    }
+}
